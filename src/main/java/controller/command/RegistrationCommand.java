0b77ae242f921674fffdcd514d2d3a2e07e50a1f
derfulim <barashenko@ukr.net>
2020-01-19 13:14:01 +0200

    add getUser method in registration command

--- src/main/java/controller/command/RegistrationCommand.java
+++ src/main/java/controller/command/RegistrationCommand.java
@@ -1,7 +1,11 @@
 package controller.command;
 
 import model.entity.NoteBook;
+import model.entity.Role;
+import model.entity.User;
 import model.util.UserValidator;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class RegistrationCommand implements Command {
     private static UserValidator userValidator = UserValidator.getInstance();
@@ -12,4 +16,17 @@ public class RegistrationCommand implements Command {
 
         return null;
     }
+
+    private User getUserFromRequest(HttpServletRequest request) {
+            User user = new User();
+            user.setLogin(request.getParameter("login"));
+            user.setRole(Role.valueOf(request.getParameter("role")));
+            user.setNameUa(request.getParameter("nameUa"));
+            user.setSurnameUa(request.getParameter("surNameUa"));
+            user.setNameEng(request.getParameter("nameEng"));
+            user.setSurnameEng(request.getParameter("surNameEng"));
+            user.setEmail(request.getParameter("email"));
+            user.setPassword(request.getParameter("password"));
+            return user;
+        }
 }
