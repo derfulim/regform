11862641ea8a3456097fa0e9bf9f311adc2a6736
derfulim <barashenko@ukr.net>
2020-01-19 13:12:16 +0200

    add fields in registration command

--- src/main/java/controller/command/RegistrationCommand.java
+++ src/main/java/controller/command/RegistrationCommand.java
@@ -1,6 +1,12 @@
 package controller.command;
 
+import model.entity.NoteBook;
+import model.util.UserValidator;
+
 public class RegistrationCommand implements Command {
+    private static UserValidator userValidator = UserValidator.getInstance();
+    private static NoteBook noteBook = new NoteBook();
+
     @Override
     public String execute() {
 
